2642e5cbfec5be9deb872736f0e65a72b79e7a80
soddong <aledjdygus9300@gmail.com>
2025-01-28 00:48:39 +0900

    Feat: S3 스토리지에서 Minio 서비스로 변경

--- backend/src/main/java/com/flolink/backend/global/config/S3Config.java
+++ backend/src/main/java/com/flolink/backend/global/config/S3Config.java
@@ -6,8 +6,10 @@ import org.springframework.context.annotation.Configuration;
 
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
-import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.S3Configuration;
+
+import java.net.URI;
 
 @Configuration
 public class S3Config {
@@ -18,16 +20,20 @@ public class S3Config {
 	@Value("${aws.s3.secret-key}")
 	private String secretKey;
 
-	@Value("${aws.s3.region}")
-	private String region;
+	@Value("${aws.s3.endpoint}")
+	private String endpoint;
 
 	@Bean
 	public S3Client s3Client() {
 		AwsBasicCredentials awsCreds = AwsBasicCredentials.create(accessKey, secretKey);
+
 		return S3Client
-			.builder()
-			.region(Region.of(region))
-			.credentialsProvider(StaticCredentialsProvider.create(awsCreds))
-			.build();
+				.builder()
+				.endpointOverride(URI.create(endpoint)) // MinIO 엔드포인트 설정
+				.credentialsProvider(StaticCredentialsProvider.create(awsCreds))
+				.serviceConfiguration(S3Configuration.builder()
+						.pathStyleAccessEnabled(true) // MinIO는 path-style을 사용
+						.build())
+				.build();
 	}
 }
